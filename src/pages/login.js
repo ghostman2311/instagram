de1fa90410b4fa307556e283ba10d8f7959075db
Nikhil <nkarnwal07@gmail.com>
2022-04-03 09:39:34 +0530

    Built Login page, SignUp page and Not Found Page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,96 @@
+import {
+  Button,
+  Card,
+  CardHeader,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
 import { useLoginPageStyles } from "../styles";
+import FacebookBlue from "../images/facebook-icon-blue.svg";
+import FacebookWhite from "../images/facebook-icon-white.png";
+
+export function LoginWithFacebook({ color, iconColor, variant }) {
+  const classes = useLoginPageStyles();
+  const facebookIcon = iconColor === "blue" ? FacebookBlue : FacebookWhite;
+
+  return (
+    <Button fullWidth color={color} variant={variant}>
+      <img
+        src={facebookIcon}
+        alt="Facebook Icon"
+        className={classes.facebookIcon}
+      />
+      Login With Facebook
+    </Button>
+  );
+}
 
 function LoginPage() {
-  useLoginPageStyles();
+  const classes = useLoginPageStyles();
 
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <section className={classes.section}>
+        <article>
+          <Card className={classes.card}>
+            <CardHeader className={classes.cardHeader} />
+            <form>
+              <TextField
+                variant="filled"
+                fullWidth
+                label="Username"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="username"
+              />
+              <TextField
+                variant="filled"
+                fullWidth
+                label="Password"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="current-password"
+              />
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                className={classes.button}
+                type="submit"
+              >
+                Login
+              </Button>
+            </form>
+            <div className={classes.orContainer}>
+              <div className={classes.orLine} />
+              <div>
+                <Typography variant="body2" color="textSecondary">
+                  OR
+                </Typography>
+                <div className={classes.orLine} />
+              </div>
+            </div>
+            <LoginWithFacebook color="secondary" iconColor="blue" />
+            <Button color="secondary" fullWidth>
+              <Typography variant="caption">Forgot Password?</Typography>
+            </Button>
+          </Card>
+          <Card className={classes.signUpCard}>
+            <Typography align="right" variant="body2">
+              Don't have an acount?
+            </Typography>
+            <Link to="/accounts/emailsignup">
+              <Button color="primary" className={classes.signUpButton}>
+                Sign Up
+              </Button>
+            </Link>
+          </Card>
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default LoginPage;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,10 +1,92 @@
 import React from "react";
 import { useSignUpPageStyles } from "../styles";
+import { LoginWithFacebook } from "./login";
+import { TextField, Button, Card, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import SEO from "../components/shared/Seo";
 
 function SignUpPage() {
-  useSignUpPageStyles();
+  const classes = useSignUpPageStyles();
 
-  return <div>SignUpPage</div>;
+  return (
+    <>
+      <SEO title="Sign Up" />
+      <section className={classes.section}>
+        <article>
+          <Card className={classes.card}>
+            <div className={classes.cardHeader} />
+            <LoginWithFacebook
+              color="primary"
+              iconColor="white"
+              variant="contained"
+            />
+            <div className={classes.orContainer}>
+              <div className={classes.orLine} />
+              <div>
+                <Typography variant="body2" color="textSecondary">
+                  OR
+                </Typography>
+                <div className={classes.orLine} />
+              </div>
+            </div>
+            <form>
+              <TextField
+                variant="filled"
+                fullWidth
+                label="Mobile Number or Email"
+                margin="dense"
+                className={classes.textField}
+                type="email"
+              />
+              <TextField
+                variant="filled"
+                fullWidth
+                label="Full Name"
+                margin="dense"
+                className={classes.textField}
+              />
+              <TextField
+                variant="filled"
+                fullWidth
+                label="Username"
+                margin="dense"
+                className={classes.textField}
+              />
+              <TextField
+                variant="filled"
+                fullWidth
+                label="Password"
+                margin="dense"
+                className={classes.textField}
+                type="password"
+                autoComplete="new-password"
+              />
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                className={classes.button}
+                type="submit"
+                disabled={true}
+              >
+                Sign Up
+              </Button>
+            </form>
+          </Card>
+          <Card className={classes.loginCard}>
+            <Typography align="right" variant="body2">
+              Have an acount?
+            </Typography>
+            <Link to="/accounts/login">
+              <Button color="primary" className={classes.loginButton}>
+                Login
+              </Button>
+            </Link>
+          </Card>
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default SignUpPage;
