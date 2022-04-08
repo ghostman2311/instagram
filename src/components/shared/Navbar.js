3602f4b5066fce863001635ae7a70651d5fea767
Nikhil <nkarnwal07@gmail.com>
2022-04-08 17:04:53 +0530

    Added Search component in navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavbarStyles } from "../../styles";
-import { AppBar } from "@material-ui/core";
+import { AppBar, Hidden, InputBase } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
+import { LoadingIcon } from "../../icons";
 
 function Logo() {
   const classes = useNavbarStyles();
@@ -17,13 +18,47 @@ function Logo() {
   );
 }
 
-function Navbar() {
+function Search() {
+  const [query, setQuery] = useState("");
+  let loading = false;
+  const classes = useNavbarStyles();
+  return (
+    <Hidden xsDown>
+      <InputBase
+        className={classes.input}
+        startAdornment={<span className={classes.searchIcon} />}
+        endAdornment={
+          loading ? (
+            <LoadingIcon />
+          ) : (
+            <span className={classes.clearIcon} onClick={() => setQuery("")} />
+          )
+        }
+        placeholder="Search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </Hidden>
+  );
+}
+
+function Links() {
+  return <>Links</>;
+}
+
+function Navbar({ minimalNavbar }) {
   const classes = useNavbarStyles();
 
   return (
     <AppBar className={classes.appBar}>
       <section className={classes.section}>
         <Logo />
+        {!minimalNavbar && (
+          <>
+            <Search />
+            <Links />
+          </>
+        )}
       </section>
     </AppBar>
   );
